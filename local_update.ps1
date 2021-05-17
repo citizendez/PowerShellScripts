a4c26f9a9041c350b1fc41b1470fe7bdd51b126e
Desiree Herschberger <citizendez@gmail.com>
2021-05-17 16:02:05 -0400

    refactored

--- local_update.ps1
+++ local_update.ps1
@@ -1,1 +1,8 @@
+cd C:\posh
+git pull;
+
 Copy-Item -path c:\posh\*.ps1 -destination C:\Users\citiz\source\repos\PowerShellScripts -Recurse 
+cd C:\Users\citiz\source\repos\PowerShellScripts
+git add .; 
+git commit -m 'refactored ' ;  
+git push;
